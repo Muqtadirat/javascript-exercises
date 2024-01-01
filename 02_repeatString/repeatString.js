74b9bf95ff6f52532dbb6f46762b8b2f02bf6d5d
Muqtadirat <muqtadir.my@gmail.com>
2024-01-01 15:33:13 +0100

    completed 01

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  // let repeat = string.repeat(num);
+  // const number = Math.floor(Math.random() * 1000)
+  if (num >= 0) {
+    return (repeat = string.repeat(num));
+  } else if (string === "") {
+    return ""
+  }
+  else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

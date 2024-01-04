48f6122544f4e12405bd37a7283dcfd6b11ec290
Muqtadirat <muqtadir.my@gmail.com>
2024-01-04 17:07:56 +0100

    completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  let total = 0;
+  
+  if (x < 0 || y < 0) {
+    total = "ERROR";
+  } else if (typeof x != "number" || typeof y != "number") {
+    total = "ERROR";
+  } else if (y > x) {
+    for (let i = x; i <= y; i++) {
+      total += i;
+    }
+  } else if (x > y) {
+    for (let i = y; i <= x; i++) {
+      total += i;
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line

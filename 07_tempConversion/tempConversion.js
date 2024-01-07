83dcf6e63c1ce48c3deb78a0f9026f28755ba074
Muqtadirat <muqtadir.my@gmail.com>
2024-01-07 13:00:13 +0100

    completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celsius = (temp - 32) * 5 / 9;
+
+    let celsiusDecimal = Number(celsius.toFixed(1))
+  return celsiusDecimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = (temp * 9) / 5 + 32;
+
+  let fahrenheitDecimal = Number(fahrenheit.toFixed(1))
+
+  return fahrenheitDecimal
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
